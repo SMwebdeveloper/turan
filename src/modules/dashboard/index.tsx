706acc731d23faa42e6f72b73967c4852a25f6c9
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2025-02-21 14:55:28 +0500

    add dashboard buttons

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+  
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="container mx-auto pt-6 px-14">
+      <h1 className="text-[32px] font-bold text-yellow-400 text-center mb-9">
+        Bir oylik ko'rsatish
+      </h1>
+      <h1 className="text-[32px] font-bold text-yellow-400 text-center mb-9">
+        Tez tugmalar
+      </h1>
+
+      <div className="grid grid-cols-2 gap-x-[35px] gap-y-[31px]">
+        <button className="bg-white/25 border border-yellow-400 rounded-xl h-[114px] flex items-center justify-center text-yellow-400 text-[26px] font-bold cursor-pointer">
+          O'quvchini natijasini qo'shish
+        </button>
+        <button className="bg-white/25 border border-yellow-400 rounded-xl h-[114px] flex items-center justify-center text-yellow-400 text-[26px] font-bold cursor-pointer">
+          Lavhalar qo'shish
+        </button>
+        <button className="bg-white/25 border border-yellow-400 rounded-xl h-[114px] flex items-center justify-center text-yellow-400 text-[26px] font-bold cursor-pointer">
+          O'qituvchini natijasini qo'shish
+        </button>
+        <button className="bg-white/25 border border-yellow-400 rounded-xl h-[114px] flex items-center justify-center text-yellow-400 text-[26px] font-bold cursor-pointer">
+          Statistikani qo'shish
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard
