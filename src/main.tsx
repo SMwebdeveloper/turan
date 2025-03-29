45cccdc09b8e8d5e3562c33d5ef49c695cfd6082
SMwebdeveloper <samandarwebdeveloper@gmail.com>
2025-03-29 22:14:57 +0500

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import Root from './router/index'
-import './index.css'
-import {Provider} from "react-redux"
-import {store} from "./store"
-createRoot(document.getElementById('root')!).render(
- 
-   <Provider store={store}>
-     <Root />
-   </Provider>
-
-)
+import { createRoot } from "react-dom/client";
+import Root from "./router/index";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <Root />
+  </Provider>
+);
